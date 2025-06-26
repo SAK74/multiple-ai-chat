e3a48485dde882cb0b10a314f5f393e400682f7d
SAK74 <sergijks@gmail.com>
2025-06-26 22:43:26 +0200

    feat: use user query in POST request

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: any) {
+export async function POST(request: NextRequest, { params }: any) {
   try {
     const chunksLenght = 10;
     let timeout: NodeJS.Timeout;
+    const queryText = await request.text();
     const stream = new ReadableStream({
       start(controller) {
         for (let i = 1; i <= chunksLenght; i += 1) {
           timeout = setTimeout(() => {
             controller.enqueue(
-              new TextEncoder().encode(`The ${i} exampled message`)
+              new TextEncoder().encode(`The ${i} ${queryText}`)
             );
             if (i === chunksLenght) {
               controller.close();
@@ -22,13 +23,18 @@ export async function GET(request: NextRequest, { params }: any) {
         timeout.close();
       },
     });
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
     return new Response(stream, {
       headers: {
         "Content-Type": "text/event-stream",
         "Cache-Control": "no-cache",
         Connection: "keep-alive",
       },
-      // status: 200,
+      status: 200,
     });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,75 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {
+  FormEventHandler,
+  ReactNode,
+  Ref,
+  RefCallback,
+  useEffect,
+  useState,
+} from "react";
 
 export default function Home() {
-  const [blocks, setBlocks] = useState<string[]>([]);
-  useEffect(() => {
-    fetch("api").then((resp) => {
+  const [blocks, setBlocks] = useState<ReactNode[]>([]);
+  // const [responseBlock, setResponseBlock] = useState("");
+  // useEffect(() => {
+  //   // fetch("api").then((resp) => {
+  //   //   const reader = resp.body
+  //   //     ?.pipeThrough(new TextDecoderStream())
+  //   //     .getReader();
+  //   //   reader?.read().then(function processData({ done, value }) {
+  //   //     console.log({ done, value });
+  //   //     if (done) {
+  //   //       return;
+  //   //     }
+  //   //     setBlocks((prev) => {
+  //   //       if (!prev.length) {
+  //   //         return [value];
+  //   //       } else {
+  //   //         const copied = [...prev];
+  //   //         copied[copied.length - 1] += value;
+  //   //         return copied;
+  //   //       }
+  //   //     });
+  //   //     reader.read().then(processData);
+  //   //   });
+  //   // });
+  // }, []);
+
+  const inputRef: RefCallback<HTMLInputElement> = (input) => {
+    input?.focus();
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
+    // todo: desactivate input element
+    const inputEl = ev.currentTarget["input"] as HTMLInputElement;
+    const inputText = inputEl.value;
+    inputEl.value = "";
+    setBlocks((prev) => {
+      const copy = [...prev];
+      copy.push(
+        inputText,
+        <span className="text-red-300 text-lg">Loading</span>
+      );
+      return copy;
+    });
+    fetch("api", {
+      method: "POST",
+      body: inputText,
+    }).then((resp) => {
+      setBlocks((prev) => {
+        const copy = [...prev];
+        copy[copy.length - 1] = "";
+        return copy;
+      });
       const reader = resp.body
         ?.pipeThrough(new TextDecoderStream())
         .getReader();
       reader?.read().then(function processData({ done, value }) {
         console.log({ done, value });
         if (done) {
+          // todo: clear and acticate input element
           return;
         }
         setBlocks((prev) => {
@@ -19,14 +77,16 @@ export default function Home() {
             return [value];
           } else {
             const copied = [...prev];
+            // copied.push("");
             copied[copied.length - 1] += value;
             return copied;
           }
         });
+        // setResponseBlock(prev=>prev.concat(value))
         reader.read().then(processData);
       });
     });
-  }, []);
+  };
 
   return (
     <div className="h-screen">
@@ -35,13 +95,26 @@ export default function Home() {
         <h1>Hello!</h1>
         <section className="grow">
           {blocks.map((text, i) => (
-            <p key={i} className="text-left">
+            <p key={i} className="text-left border rounded">
               {text}
             </p>
           ))}
         </section>
-        <form>
-          <input type="text" placeholder="Start typing here" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Start typing here"
+            name="input"
+            ref={inputRef}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              setBlocks([]);
+            }}
+          >
+            Clear
+          </button>
         </form>
       </main>
     </div>
