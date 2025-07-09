dd30eb1bcaf1595186e63535c1fef7b8fe5e2b81
SAK74 <sergijks@gmail.com>
2025-07-09 21:35:20 +0200

    build: initiate sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,17 @@
-"use client";
+import { cookies } from "next/headers";
+import { SidebarProvider } from "../components/ui/sidebar";
+import { SideBarComp } from "./_components/SideBar";
+import { Chat } from "./_components/Chat";
+import { SideBarTrigger } from "./_components/SideTrigger";
 
-import { useChat } from "@ai-sdk/react";
-import { useAssistant, useUsage } from "./_components/hooks/localStorage.hook";
-import {
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-  type FormEventHandler,
-} from "react";
-import type { ModelId, Provider } from "./types";
-import { ControllPanel } from "./_components/Controll";
-
-import { TOKENS_LIMIT } from "./_constants";
-import { showOverdraft } from "./_tools/overdraftMessage";
-import { PromtForm, RenderMessages, Spinner, Usage } from "./_components";
-
-export default function Page() {
-  const { usage, setUsage } = useUsage();
-  const { assystentDescription } = useAssistant();
-  const [provider, setProvider] = useState<Provider>();
-  const [model, setModel] = useState<ModelId | undefined>();
-  const [apiKey, setApiKey] = useState<string | undefined>();
-
-  const chat = useChat({
-    api: "/api/aichat",
-    onFinish(_, options) {
-      const summary = usage + options.usage.totalTokens;
-      setUsage(summary);
-      if (summary >= TOKENS_LIMIT) {
-        showOverdraft();
-      }
-      // bottomRef.current?.scrollIntoView();
-    },
-    body: {
-      system: assystentDescription,
-      provider,
-      model,
-      apiKey,
-    },
-  });
-
-  const {
-    messages,
-    handleSubmit,
-    setMessages,
-    error,
-    status,
-    data,
-    setData,
-    reload: chatReload,
-  } = chat;
-  const isActive = useMemo(
-    () => usage < TOKENS_LIMIT || Boolean(apiKey),
-    [usage, apiKey]
-  );
-
-  const [streamStatus, setStreamStatus] = useState<string>();
-  useEffect(() => {
-    const current = data?.at(-1);
-    if (typeof current === "string") {
-      setStreamStatus(current);
-    }
-  }, [data?.length]);
-
-  const bottomRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView();
-  }, [messages]);
-
-  const onQuerySubmit: FormEventHandler = (ev) => {
-    if (!isActive) {
-      ev.preventDefault();
-      showOverdraft();
-      return;
-    }
-    handleSubmit(ev);
-    setData(undefined);
-  };
-
+export default async function Page() {
+  const cookiesState = await cookies();
+  const isSidebarOpened = cookiesState.get("sidebar_state")?.value === "true";
   return (
-    <div className="px-6">
-      <ControllPanel className="py-3" {...{ apiKey, setApiKey }}>
-        {!apiKey && <Usage className="" />}
-      </ControllPanel>
-      <RenderMessages messages={messages} setMessages={setMessages} />
-      <div ref={bottomRef} className="h-4" />
-      {status === "submitted" && (
-        <div className="inline-block">
-          <Spinner />
-        </div>
-      )}
-      {status === "streaming" && <div>{streamStatus}</div>}
-      {status === "error" && (
-        <p className="text-destructive/85">{error?.message}</p>
-      )}
-
-      <PromtForm
-        {...{
-          ...chat,
-          provider,
-          setProvider,
-          model,
-          setModel,
-          isActive,
-          onQuerySubmit,
-          reload: () => {
-            setData(undefined);
-            return chatReload();
-          },
-        }}
-      />
-    </div>
+    <SidebarProvider defaultOpen={isSidebarOpened}>
+      <SideBarComp />
+      <SideBarTrigger />
+      <Chat />
+    </SidebarProvider>
   );
 }
